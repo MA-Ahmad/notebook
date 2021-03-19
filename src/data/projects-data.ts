c90739b375b6efc685ae3e4343ba6498367501f5
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-19 03:26:18 -0700

    Create projects-data.ts

---
+++ src/data/projects-data.ts
@@ -0,0 +1,40 @@
+const projectsData = [
+    {
+        title: "Portfolio",
+        desc: "Personal portfolio app to show my skills and experience.",
+        technologies: ['React', 'ChakraUi'],
+        stars: 2,
+        githubLink: "https://github.com/MA-Ahmad/portfolio",
+        liveLink: "https://mahmad.me/",
+        coverImage: '/cover_images/portfolio.png'
+      },
+  {
+    title: "Blog App",
+    desc: "A Blog App built by using React, ChakraUI and Formik.",
+    technologies: ['React', 'ChakraUi'],
+    stars: 9,
+    githubLink: "https://github.com/MA-Ahmad/reactBlog",
+    liveLink: "https://ma-ahmad.github.io/reactBlog",
+    coverImage: '/cover_images/blog_1.png'
+  },
+  {
+    title: "Blog App + Authentication",
+    desc: "A Blog App built by using React, Rails, ChakraUI and Formik.",
+    technologies: ['React', 'Rails', 'ChakraUi'],
+    stars: 6,
+    githubLink: "https://github.com/MA-Ahmad/blog-app-react-frontend",
+    liveLink: "https://blog-frontend-react.herokuapp.com/",
+    coverImage: '/cover_images/blog_2.png'
+  },
+  {
+    title: "CRUD App",
+    desc: "A simple react+rails(RR) CRUD app with tailwindcss.",
+    technologies: ['React', 'Rails', 'Tailwindcss'],
+    stars: 1,
+    githubLink: "https://github.com/MA-Ahmad/react_rails_blog",
+    liveLink: "https://react-on-rails-blog.herokuapp.com/",
+    coverImage: '/cover_images/blog_3.png'
+  },
+];
+
+export default projectsData;
