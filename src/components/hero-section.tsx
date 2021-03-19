a07c1f988a61ec71c25316f8db36a73e14f8f110
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-19 14:19:18 -0700

    Update hero-section.tsx

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,27 +1,26 @@
-import * as React from 'react';
-import {
-  Heading,
-  Text,
-  Flex
-} from '@chakra-ui/react';
+import * as React from "react";
+import { Heading, Text, Flex } from "@chakra-ui/react";
 
 export default function HeroSection() {
   return (
     <>
-        <Flex
-        h={"50vh"}
-          justifyContent="center"
-          align="center">
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
-            lineHeight={'110%'}>
-            Make notes for <br />
-            <Text as={'span'} color={'green.400'}>
-              your daily work
-            </Text>
-          </Heading>
-        </Flex>
+      <Flex h={"50vh"} justifyContent="center" align="center">
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+          lineHeight={"110%"}
+        >
+          Make notes for <br />
+          <Text
+            as={"span"}
+            color={"green.400"}
+            bgGradient="linear(to-r, green.200, pink.500)"
+            bgClip="text"
+          >
+            your daily work
+          </Text>
+        </Heading>
+      </Flex>
     </>
   );
 }
