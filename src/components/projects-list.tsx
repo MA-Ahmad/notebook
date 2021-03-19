909643f16b5b0e9acfbfbab0827b88fcdc4b8e0f
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-19 14:20:13 -0700

    Update projects-list.tsx

--- src/components/projects-list.tsx
+++ src/components/projects-list.tsx
@@ -5,40 +5,31 @@ import {
   Image,
   Badge,
   SimpleGrid,
-  List,
-  ListItem,
-  ListIcon,
   Flex,
   Link,
-  Text,
-  Tooltip,
-  HStack
+  HStack,
+  useColorModeValue
 } from "@chakra-ui/react";
 import { StarIcon, ExternalLinkIcon } from "@chakra-ui/icons";
 import projectsData from "../data/projects-data";
-export interface ProjectsListProps {}
 
-const ProjectsList: React.SFC<ProjectsListProps> = () => {
-  const property = {
-    imageUrl: "https://bit.ly/2Z4KKcF",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Blog App",
-    detail: "A React Blog App built by using libraries like ChakraUI, Formik",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4
-  };
-
+const ProjectsList = () => {
+  const bg = useColorModeValue("white", "#2f3244");
+  const textColor = useColorModeValue("gray.600","#b5b1b1");
+  
   return (
     <Box minH={"50vh"}>
       <Flex p="2" justifyContent="center">
         <Heading
           as="h1"
           size="xl"
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip="text"
           _focus={{ boxShadow: "none", outline: "none" }}
-          _hover={{ textDecoration: "none", color: "yellow.400" }}
+          _hover={{
+            textDecoration: "none",
+            bgGradient: "linear(to-r, red.500, yellow.500)"
+          }}
         >
           Projects
         </Heading>
@@ -50,26 +41,28 @@ const ProjectsList: React.SFC<ProjectsListProps> = () => {
         position="relative"
         overflow="hidden"
       >
-        {projectsData.map(project => (
+        {projectsData.map((project, index) => (
           <Box
             maxW="sm"
-          height="fit-content"
+            height="fit-content"
             borderWidth="1px"
             borderRadius="lg"
             overflow="hidden"
             boxShadow="md"
             mx="5px"
             mb="20px"
+            _hover={{ boxShadow: "lg" }}
+            key={index}
           >
             <Image
-              height={{sm: "20vh",md: "20vh",lg: "25.5vh", xl: '25.5vh'}}
+              height={{ sm: "20vh", md: "20vh", lg: "27vh", xl: "27vh" }}
               width={"100%"}
               src={project.coverImage}
               objectFit="cover"
               alt="cover image"
               fallbackSrc="https://via.placeholder.com/320x216/DCDFDF/ffffff/?text=CoverImage"
             />
-            <Box p="5">
+            <Box p="5" bg={bg}>
               <Flex justifyContent="space-between" alignItems="center">
                 <Box
                   mt="1"
@@ -81,7 +74,7 @@ const ProjectsList: React.SFC<ProjectsListProps> = () => {
                   isTruncated
                 >
                   <Link
-                    href={project.liveLink}
+                    href={project.liveLink || project.githubLink}
                     textDecoration={"none !important"}
                     isExternal
                   >
@@ -90,32 +83,37 @@ const ProjectsList: React.SFC<ProjectsListProps> = () => {
                   </Link>
                 </Box>
                 <Box>
-                  <Link
-                    href={project.githubLink}
-                    textDecoration={"none !important"}
-                    isExternal
-                  >
-                    <StarIcon color="teal.300" fontSize="sm" />
-                    <Box as="span" ml="2" color="gray.600" fontSize="sm">
-                      {/* <Tooltip
+                  {project.stars ? (
+                    <Link
+                      href={project.githubLink}
+                      textDecoration={"none !important"}
+                      isExternal
+                    >
+                      <StarIcon color="teal.300" fontSize="sm" />
+                      <Box as="span" ml="2" color={textColor} fontSize="sm">
+                        {/* <Tooltip
                         hasArrow
                         label="Github stars"
                         placement="top-end"
                       > */}
-                      {project.stars}
-                      {/* </Tooltip> */}
-                    </Box>
-                  </Link>
+                        {project.stars}
+                        {/* </Tooltip> */}
+                      </Box>
+                    </Link>
+                  ) : (
+                    ""
+                  )}
                 </Box>
               </Flex>
               <Box textAlign="left">
                 <HStack spacing="1" mt="2" mb="2">
-                  {project.technologies.map(tech => (
+                  {project.technologies.map((tech, index) => (
                     <Badge
                       borderRadius="full"
                       px="1"
                       colorScheme="teal"
                       textTransform="lowercase"
+                      key={index}
                     >
                       {tech}
                     </Badge>
@@ -123,73 +121,12 @@ const ProjectsList: React.SFC<ProjectsListProps> = () => {
                 </HStack>
               </Box>
 
-              <Box color="gray.600" fontSize="md" textAlign="left">
+              <Box color={textColor} fontSize="md" textAlign="left">
                 {project.desc}
               </Box>
-
             </Box>
           </Box>
         ))}
-        <Box
-          maxW="sm"
-          height="fit-content"
-          borderWidth="1px"
-          borderRadius="lg"
-          overflow="hidden"
-          boxShadow="md"
-          mx="5px"
-          mb="20px"
-        >
-          <Image
-            src={property.imageUrl}
-            alt={property.imageAlt}
-            fallbackSrc="https://via.placeholder.com/320x216/DCDFDF/ffffff/?text=CoverImage"
-          />
-
-          <Box p="5">
-            <Flex justifyContent="space-between" alignItems="center">
-              <Box
-                mt="1"
-                fontWeight="semibold"
-                as="h4"
-                lineHeight="tight"
-                textAlign="left"
-                isTruncated
-              >
-                <Link
-                  href="https://chakra-ui.com"
-                  textDecoration={"none !important"}
-                  isExternal
-                >
-                  {property.title}
-                  <ExternalLinkIcon mx="3px" />
-                </Link>
-              </Box>
-              <Box>
-                <Link
-                  href="https://chakra-ui.com"
-                  textDecoration={"none !important"}
-                  isExternal
-                >
-                  <StarIcon color="teal.300" fontSize="sm" />
-                  <Box as="span" ml="2" color="gray.600" fontSize="sm">
-                    <Tooltip hasArrow label="Github stars" placement="top-end">
-                      9
-                    </Tooltip>
-                  </Box>
-                </Link>
-              </Box>
-            </Flex>
-            <Box textAlign="left">
-              <Badge borderRadius="full" px="1" colorScheme="teal">
-                React
-              </Badge>
-            </Box>
-            <Box color="gray.600" fontSize="md" textAlign="left">
-              {property.detail}
-            </Box>
-          </Box>
-        </Box>
       </SimpleGrid>
     </Box>
   );
