a5c7c9f119108e7e154071176a5af32e43c0c21b
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-19 14:19:31 -0700

    Create home-page.tsx

---
+++ src/components/home-page.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { useDisclosure } from "@chakra-ui/react";
+import NotesList from "./notes-list";
+import HeroSection from "./hero-section";
+import NoteForm from "./note-form";
+
+export interface HomePageProps {
+  notes: note[];
+  setNotes: (note: note[]) => void;
+}
+
+export const HomePage: React.SFC<HomePageProps> = ({ notes, setNotes }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedNote, setSelectedNote] = React.useState<note>();
+
+  // const handleNoteCreate = (note: note) => {
+  //   const newNotesState: note[] = [...notes];
+  //   newNotesState.push(note);
+  //   setNotes(newNotesState);
+  // };
+
+  const handleClick = (id: string) => {
+    const selectedNote = notes.find((note: note) => note.id === id);
+    setSelectedNote(selectedNote);
+    onOpen();
+  };
+
+  const handleNoteUpdate = (newNote: note) => {
+    const newNotesState: note[] = [...notes];
+    const index = notes.findIndex((note: note) => note.id === newNote.id);
+    newNotesState[index] = newNote;
+    setNotes(newNotesState);
+  };
+
+  return (
+    <>
+      {notes.length ? (
+        <NotesList notes={notes} handleClick={handleClick} />
+      ) : (
+        <HeroSection />
+      )}
+      {isOpen ? (
+        <NoteForm
+          isOpen={isOpen}
+          onClose={onClose}
+          handleNoteUpdate={handleNoteUpdate}
+          selectedNote={selectedNote}
+        />
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
