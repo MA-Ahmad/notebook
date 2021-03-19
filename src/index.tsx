8f764a3d2b272539273dd108685646cbd6df6e87
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-19 03:26:11 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,14 @@
-import { Box, ColorModeScript } from "@chakra-ui/react"
-import * as React from "react"
-import ReactDOM from "react-dom"
-import { App } from "./App"
-import reportWebVitals from "./reportWebVitals"
-import * as serviceWorker from "./serviceWorker"
+import { Box, ColorModeScript } from "@chakra-ui/react";
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript />
-    <Box maxW='1050px' mx='auto'>
+    <Box maxW="1050px" mx="auto">
       <App />
     </Box>
   </React.StrictMode>,
-  document.getElementById("root"),
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+  document.getElementById("root")
+);
