b70b42e1ee618b9a1f68e5b490f2e3031de87d21
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-19 08:56:47 -0700

    Update projects-data.ts

--- src/data/projects-data.ts
+++ src/data/projects-data.ts
@@ -1,40 +1,58 @@
 const projectsData = [
-    {
-        title: "Portfolio",
-        desc: "Personal portfolio app to show my skills and experience.",
-        technologies: ['React', 'ChakraUi'],
-        stars: 2,
-        githubLink: "https://github.com/MA-Ahmad/portfolio",
-        liveLink: "https://mahmad.me/",
-        coverImage: '/cover_images/portfolio.png'
-      },
   {
     title: "Blog App",
     desc: "A Blog App built by using React, ChakraUI and Formik.",
-    technologies: ['React', 'ChakraUi'],
+    technologies: ["React", "ChakraUi"],
     stars: 9,
     githubLink: "https://github.com/MA-Ahmad/reactBlog",
     liveLink: "https://ma-ahmad.github.io/reactBlog",
-    coverImage: '/cover_images/blog_1.png'
+    coverImage: "/cover_images/blog_1.png"
   },
   {
     title: "Blog App + Authentication",
     desc: "A Blog App built by using React, Rails, ChakraUI and Formik.",
-    technologies: ['React', 'Rails', 'ChakraUi'],
+    technologies: ["React", "Rails", "ChakraUi"],
     stars: 6,
     githubLink: "https://github.com/MA-Ahmad/blog-app-react-frontend",
     liveLink: "https://blog-frontend-react.herokuapp.com/",
-    coverImage: '/cover_images/blog_2.png'
+    coverImage: "/cover_images/blog_2.png"
   },
   {
-    title: "CRUD App",
+    title: "Portfolio",
+    desc: "Personal portfolio app to show my skills and experience.",
+    technologies: ["React", "ChakraUi"],
+    stars: 2,
+    githubLink: "https://github.com/MA-Ahmad/portfolio",
+    liveLink: "https://mahmad.me/",
+    coverImage: "/cover_images/portfolio.png"
+  },
+  {
+    title: "Crud Demo App",
     desc: "A simple react+rails(RR) CRUD app with tailwindcss.",
-    technologies: ['React', 'Rails', 'Tailwindcss'],
+    technologies: ["React", "Rails", "Tailwindcss"],
     stars: 1,
     githubLink: "https://github.com/MA-Ahmad/react_rails_blog",
     liveLink: "https://react-on-rails-blog.herokuapp.com/",
-    coverImage: '/cover_images/blog_3.png'
+    coverImage: "/cover_images/blog_3.png"
   },
+  {
+    title: "Scrapper App",
+    desc: "A simple rails scrapper app to count html tags of a web page.",
+    technologies: ["Rails"],
+    stars: 1,
+    githubLink: "https://github.com/MA-Ahmad/rails-app",
+    liveLink: "https://urltohtmlapp.herokuapp.com/",
+    coverImage: ""
+  },
+  {
+    title: "Image Gallery",
+    desc: "This app built by using Tailwind CSS with React and Pixabay API.",
+    technologies: ["React", "Tailwindcss"],
+    stars: 0,
+    githubLink: "https://github.com/MA-Ahmad/react-image-gallery",
+    liveLink: "",
+    coverImage: "/cover_images/image_gallery.png"
+  }
 ];
 
 export default projectsData;
