e9c4e4460db014287e0c66e271cd813791762159
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-21 01:43:46 -0700

    Create cover_images.ts

---
+++ src/data/cover_images.ts
@@ -0,0 +1,10 @@
+const coverImages = [
+  "/cover_images/blog_1.png",
+  "/cover_images/blog_2.png",
+  "/cover_images/portfolio.png",
+  "/cover_images/image_gallery.png",
+  "/cover_images/blog_3.png",
+  "/cover_images/notebook_app.png"
+];
+
+export default coverImages;
