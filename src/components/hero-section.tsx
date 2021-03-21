d73260ed769c99695f334bab07944188087d9e8f
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-21 01:49:32 -0700

    Update hero-section.tsx

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -5,21 +5,21 @@ export default function HeroSection() {
   return (
     <>
       <Flex h={"50vh"} justifyContent="center" align="center">
-        <Heading
-          fontWeight={600}
-          fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
-          lineHeight={"110%"}
-        >
-          Make notes for <br />
-          <Text
-            as={"span"}
-            color={"green.400"}
-            bgGradient="linear(to-r, green.200, pink.500)"
-            bgClip="text"
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
           >
-            your daily work
-          </Text>
-        </Heading>
+            <Text textShadow="1px 1px #9c1786">Make notes for </Text>
+            <Text
+              as={"span"}
+              color={"green.400"}
+              bgGradient="linear(to-r, green.200, pink.500)"
+              bgClip="text"
+            >
+              your daily work
+            </Text>
+          </Heading>
       </Flex>
     </>
   );
