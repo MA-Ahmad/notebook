0cd259e48cf68ffe65bcb022a42afefdd3a891c4
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-22 13:10:59 -0700

    added hover motion effect

--- src/components/repositories.tsx
+++ src/components/repositories.tsx
@@ -17,6 +17,7 @@ import {
 import { StarIcon, ExternalLinkIcon } from "@chakra-ui/icons";
 import repositoriesList from "../data/repositories-list";
 import Carousel from "./carousel";
+import { motion } from "framer-motion";
 
 const Repositories = () => {
   const [imageUrl, setImageUrl] = React.useState<string>("");
@@ -58,98 +59,101 @@ const Repositories = () => {
             overflow="hidden"
           >
             {repositoriesList.map((repo, index) => (
-              <Box
-                maxW="sm"
-                height="fit-content"
-                borderWidth="1px"
-                borderRadius="lg"
-                overflow="hidden"
-                boxShadow="md"
-                mx="5px"
-                mb="20px"
-                _hover={{ boxShadow: "lg" }}
-                key={index}
-              >
-                <Image
-                  height={{ base: "28vh", lg: "32vh" }}
-                  cursor="pointer"
-                  width={"100%"}
-                  src={repo.coverImage}
-                  objectFit="cover"
-                  alt="cover image"
-                  fallbackSrc="https://via.placeholder.com/320x216/DCDFDF/ffffff/?text=CoverImage"
-                  onClick={() => handleClick(repo.coverImage)}
-                />
-                <Box p="5" bg={bg}>
-                  <Flex justifyContent="space-between" alignItems="center">
-                    <Box
-                      mt="1"
-                      fontWeight="semibold"
-                      as="h6"
-                      fontSize="md"
-                      lineHeight="tight"
-                      textAlign="left"
-                      isTruncated
-                    >
-                      <Link
-                        href={repo.liveLink || repo.githubLink}
-                        textDecoration={"none !important"}
-                        isExternal
+              <motion.div whileHover={{ y: -10 }}>
+                <Box
+                  maxW="sm"
+                  height="fit-content"
+                  borderWidth="1px"
+                  borderRadius="lg"
+                  overflow="hidden"
+                  boxShadow="md"
+                  mx="5px"
+                  mb="10px"
+                  mt="10px"
+                  _hover={{ boxShadow: "lg" }}
+                  key={index}
+                >
+                  <Image
+                    height={{ base: "28vh", lg: "32vh" }}
+                    cursor={repo.coverImage ? "pointer" : "auto"}
+                    width={"100%"}
+                    src={repo.coverImage}
+                    objectFit="cover"
+                    alt="cover image"
+                    fallbackSrc="https://via.placeholder.com/320x216/DCDFDF/ffffff/?text=CoverImage"
+                    onClick={() => handleClick(repo.coverImage)}
+                  />
+                  <Box p="5" bg={bg}>
+                    <Flex justifyContent="space-between" alignItems="center">
+                      <Box
+                        mt="1"
+                        fontWeight="semibold"
+                        as="h6"
+                        fontSize="md"
+                        lineHeight="tight"
+                        textAlign="left"
+                        isTruncated
                       >
-                        {repo.title}
-                        <ExternalLinkIcon mx="3px" />
-                      </Link>
-                    </Box>
-                    <Box mt="1">
-                      {repo.stars ? (
                         <Link
-                          href={repo.githubLink}
+                          href={repo.liveLink || repo.githubLink}
                           textDecoration={"none !important"}
                           isExternal
                         >
-                          <Tooltip
-                            hasArrow
-                            label="Github stars"
-                            placement="top-end"
-                          >
-                            <Box>
-                              <StarIcon color="teal.300" fontSize="sm" />
-                              <Box
-                                as="span"
-                                ml="2"
-                                color={textColor}
-                                fontSize="sm"
-                              >
-                                {repo.stars}
-                              </Box>
-                            </Box>
-                          </Tooltip>
+                          {repo.title}
+                          <ExternalLinkIcon mx="3px" />
                         </Link>
-                      ) : (
-                        ""
-                      )}
+                      </Box>
+                      <Box mt="1">
+                        {repo.stars ? (
+                          <Link
+                            href={repo.githubLink}
+                            textDecoration={"none !important"}
+                            isExternal
+                          >
+                            <Tooltip
+                              hasArrow
+                              label="Github stars"
+                              placement="top-end"
+                            >
+                              <Box>
+                                <StarIcon color="teal.300" fontSize="sm" />
+                                <Box
+                                  as="span"
+                                  ml="2"
+                                  color={textColor}
+                                  fontSize="sm"
+                                >
+                                  {repo.stars}
+                                </Box>
+                              </Box>
+                            </Tooltip>
+                          </Link>
+                        ) : (
+                          ""
+                        )}
+                      </Box>
+                    </Flex>
+                    <Box textAlign="left">
+                      <HStack spacing="1" mt="2" mb="2">
+                        {repo.technologies.map((tech, index) => (
+                          <Badge
+                            borderRadius="full"
+                            px="1"
+                            colorScheme={tech.color}
+                            textTransform="lowercase"
+                            key={index}
+                          >
+                            {tech.name}
+                          </Badge>
+                        ))}
+                      </HStack>
                     </Box>
-                  </Flex>
-                  <Box textAlign="left">
-                    <HStack spacing="1" mt="2" mb="2">
-                      {repo.technologies.map((tech, index) => (
-                        <Badge
-                          borderRadius="full"
-                          px="1"
-                          colorScheme={tech.color}
-                          textTransform="lowercase"
-                          key={index}
-                        >
-                          {tech.name}
-                        </Badge>
-                      ))}
-                    </HStack>
-                  </Box>
-                  <Box color={textColor} fontSize="md" textAlign="left">
-                    {repo.desc}
+                    <Box color={textColor} fontSize="md" textAlign="left">
+                      {repo.desc}
+                    </Box>
                   </Box>
                 </Box>
-              </Box>
+              </motion.div>
             ))}
           </SimpleGrid>
         </SlideFade>
