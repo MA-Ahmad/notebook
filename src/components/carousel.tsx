709bcb9275d3c58b7d5356cacaa15a8ae13a60b2
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-22 12:40:09 -0700

    slider updated with framer-motion

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -4,12 +4,38 @@ import {
   Modal,
   ModalBody,
   ModalOverlay,
-  ModalCloseButton,
-  Image,
   IconButton
 } from "@chakra-ui/react";
 import { ChevronRightIcon, ChevronLeftIcon } from "@chakra-ui/icons";
 import coverImages from "../data/cover_images";
+import { motion, AnimatePresence } from "framer-motion";
+import "../assets/stylesheets/carousel.css";
+
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0
+    };
+  }
+};
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity;
+};
 
 export interface CarouselProps {
   onOpen: () => void;
@@ -24,63 +50,90 @@ const Carousel: React.SFC<CarouselProps> = ({
   onClose,
   isOpen
 }) => {
-  const [imageIndex, setImageIndex] = React.useState<number>(0);
+  const [[page, direction], setPage] = React.useState([0, 0]);
+  const [imageIndex1, setImageIndex1] = React.useState<number>(0);
+
+  const paginate = (newDirection: number) => {
+    setPage([page + newDirection, newDirection]);
+  };
 
   React.useEffect(() => {
     console.log(imageUrl);
     const index = coverImages.findIndex(
       (cImage: string) => cImage === imageUrl
     );
-    setImageIndex(index);
+    setImageIndex1(index);
   }, [imageUrl]);
 
-  const nextImage = () => {
-    setImageIndex(imageIndex + 1 < coverImages.length ? imageIndex + 1 : 0);
+  const nextImage = (newDirection: number) => {
+    paginate(newDirection);
+    setImageIndex1(imageIndex1 + 1 < coverImages.length ? imageIndex1 + 1 : 0);
   };
 
-  const prevImage = () => {
-    setImageIndex(0 === imageIndex ? coverImages.length - 1 : imageIndex - 1);
-    console.log(imageIndex);
+  const prevImage = (newDirection: number) => {
+    paginate(newDirection);
+    setImageIndex1(
+      0 === imageIndex1 ? coverImages.length - 1 : imageIndex1 - 1
+    );
   };
 
   return (
     <Modal isCentered onClose={onClose} size={"6xl"} isOpen={isOpen}>
       <ModalOverlay />
       <ModalContent>
-        <ModalCloseButton />
         <ModalBody padding="0">
-          <Image
-            width={"100%"}
-            height={"100%"}
-            src={coverImages[imageIndex]}
-            borderRadius="5px"
-            alt="cover image"
-            fallbackSrc="https://via.placeholder.com/320x216/DCDFDF/ffffff/?text=CoverImage"
-          />
-          <IconButton
-            aria-label="left image"
-            icon={<ChevronLeftIcon />}
-            cursor="pointer"
-            as={ChevronLeftIcon}
-            size="sm"
-            colorScheme="teal"
-            position="absolute"
-            top="50%"
-            left="5px"
-            onClick={() => prevImage()}
-          />
-          <IconButton
-            aria-label="right image"
-            icon={<ChevronRightIcon />}
-            cursor="pointer"
-            as={ChevronRightIcon}
-            size="sm"
-            colorScheme="teal"
-            position="absolute"
-            top="50%"
-            right="5px"
-            onClick={() => nextImage()}
-          />
+          <div className="carousel-container">
+            <AnimatePresence initial={false} custom={direction}>
+              <motion.img
+                key={page}
+                src={coverImages[imageIndex1]}
+                custom={direction}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={{
+                  x: { type: "spring", stiffness: 300, damping: 30 },
+                  opacity: { duration: 0.2 }
+                }}
+                drag="x"
+                dragConstraints={{ left: 0, right: 0 }}
+                dragElastic={1}
+                onDragEnd={(e, { offset, velocity }) => {
+                  const swipe = swipePower(offset.x, velocity.x);
+
+                  if (swipe < -swipeConfidenceThreshold) {
+                    paginate(1);
+                  } else if (swipe > swipeConfidenceThreshold) {
+                    paginate(-1);
+                  }
+                }}
+              />
+            </AnimatePresence>
+            <div className="next" onClick={() => nextImage(1)}>
+              <IconButton
+                aria-label="left image"
+                icon={<ChevronLeftIcon />}
+                cursor="pointer"
+                as={ChevronRightIcon}
+                size="md"
+                colorScheme="teal"
+                borderRadius="full"
+              />
+            </div>
+
+            <div className="prev" onClick={() => prevImage(-1)}>
+              <IconButton
+                aria-label="right image"
+                icon={<ChevronRightIcon />}
+                cursor="pointer"
+                as={ChevronLeftIcon}
+                size="md"
+                colorScheme="teal"
+                borderRadius="full"
+              />
+            </div>
+          </div>
         </ModalBody>
       </ModalContent>
     </Modal>
