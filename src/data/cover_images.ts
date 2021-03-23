ad4d6a3124ac42a5bf6f05acc0bf4d5f930a1a5f
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-23 12:50:58 -0700

    updated cover_images path

--- src/data/cover_images.ts
+++ src/data/cover_images.ts
@@ -1,10 +1,17 @@
+import img1 from "../assets/images/cover_images/blog_1.png";
+import img2 from "../assets/images/cover_images/blog_2.png";
+import img3 from "../assets/images/cover_images/portfolio.png";
+import img4 from "../assets/images/cover_images/image_gallery.png";
+import img5 from "../assets/images/cover_images/notebook_app.png";
+import img6 from "../assets/images/cover_images/blog_3.png";
+
 const coverImages = [
-  "/cover_images/blog_1.png",
-  "/cover_images/blog_2.png",
-  "/cover_images/portfolio.png",
-  "/cover_images/image_gallery.png",
-  "/cover_images/notebook_app.png",
-  "/cover_images/blog_3.png"
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6
 ];
 
 export default coverImages;
