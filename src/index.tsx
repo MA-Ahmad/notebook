7a9d1d041eb01ec8e8fe42d579dc9a3d05fd635e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-02 14:44:51 -0700

    changed BrowserRouter with HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { Box, ColorModeScript } from "@chakra-ui/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import * as React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
